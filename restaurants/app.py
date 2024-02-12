db7b28f0f84f8ca5bb7817a4aab6785de4e7c4a3
Michaelangelo Caporale <mvacaporale@gmail.com>
2024-02-12 13:43:47 -0500

    Add tests for invalid inputs to the API.

--- restaurants/app.py
+++ restaurants/app.py
@@ -1,22 +1,30 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
+from .utils import is_isoformat, ISOFORMAT
+
 app = Flask(__name__)
 api = Api(app)
 
 
 class Restaurants(Resource):
-    
+
     def get(self):
+
+        # Add one argument for a datetime str.
         parser = reqparse.RequestParser()
         parser.add_argument("datetime", location="args", required=True)
         args = parser.parse_args()
 
-        # TODO: Handle error cases.
-        # Return for error codes https://restfulapi.net/http-status-codes/
-
+        # Validate datetime input format.
         datetime = args['datetime']
-        return {"data": datetime}, 200
+        if not is_isoformat(datetime):
+            return {
+                "message": f"Invalid datetime format. Should be {ISOFORMAT}."
+            }, 400
+
+        # Return dummy value for now.
+        return {"data": ["Store 1", "Store_2"]}, 200
 
 api.add_resource(Restaurants, '/restaurants')
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,6 +2,7 @@ import json
 import unittest
 
 from restaurants.app import app
+from restaurants.utils import ISOFORMAT
 
 
 class RestaurantsTest(unittest.TestCase):
@@ -10,15 +11,48 @@ class RestaurantsTest(unittest.TestCase):
             self.app = app.test_client()
             # self.db = db.get_db()
 
-    def test_get_datetime(self):
+    def test_missing_parameter(self):
+        """
+        Test bad request for missing datetime parameter.
+        """
+
+        response = self.app.get("/restaurants")
+        message = response.json["message"]
+        self.assertEqual(400, response.status_code)
+        self.assertEqual(
+             "Missing required parameter in the query string",
+             message["datetime"]
+        )
+
+    def test_invalid_isoformat(self):
+        """
+        Test bad request for invalid datetime formatting.
+        """
 
         payload = {
             "datetime": "2000-10-31T01:30:00",
         }
+        response = self.app.get("/restaurants", query_string=payload)
 
-        response = self.app.get("/restaurants", query_string=payload)#, headers=headers)
+        message = response.json["message"]
+        self.assertEqual(400, response.status_code)
+        self.assertEqual(
+             f"Invalid datetime format. Should be {ISOFORMAT}.",
+             message
+        )
 
-        self.assertEqual("2000-10-31T01:30:00", response.json["data"])
+    def test_get_open_restaurants(self):
+        """
+        Test for temp dummy output from API.
+        """
+
+        payload = {
+            "datetime": "2000-10-31T01:30",
+        }
+        response = self.app.get("/restaurants", query_string=payload)
+
+        data = response.json["data"]
+        self.assertEqual(["Store 1", "Store_2"], data)
         self.assertEqual(200, response.status_code)
 
 
