6c0d22c475096238c7552d32dbe2440da8bf4abd
Michaelangelo Caporale <mvacaporale@gmail.com>
2024-02-12 20:24:39 -0500

    Handle edge case in get_open_restaurants where the store is open late into the  night.

--- restaurants/data.py
+++ restaurants/data.py
@@ -7,6 +7,7 @@ import pandas as pd
 from restaurants.utils import (
     is_isoformat,
     parse_and_tabulate_hours,
+    add_24_hours,
     CLOSES,
     OPENS,
     WEEKDAYS,
@@ -57,11 +58,32 @@ class RestaurantData:
         if not is_isoformat(datetime_str):
             raise ValueError("Expected 'datetime_str' str in isoformat.")
 
+        # This will make the code below more compact.
+        df = self.dataframe
+
+        # Split the iso format into the date and time.
         day, time = datetime_str.split("T")
         day = date.fromisoformat(day)
         weekday = WEEKDAYS[day.weekday()]
-        after_opens = self.dataframe[f"{weekday} {OPENS}"] < time
-        before_closes = self.dataframe[f"{weekday} {CLOSES}"] > time
 
-        all_open = self.dataframe[after_opens & before_closes]
-        return list(all_open.loc[:,"Restaurant Name"])
+        # Find all restaurants open at the given time.
+        after_opens1 = df[f"{weekday} {OPENS}"] <= time
+        before_closes1 = df[f"{weekday} {CLOSES}"] > time
+
+        # Find the equivalent time for the previous day.
+        # For example, Sun at 01:00 become Mon at 25:00
+        time_plus_24 = add_24_hours(time)
+        previous_day = WEEKDAYS[(day.weekday() - 1) % 7]
+
+        # Find all restaurants open at the transformed time.
+        after_opens2 = df[f"{previous_day} {OPENS}"] <= time_plus_24
+        before_closes2 = df[f"{previous_day} {CLOSES}"] > time_plus_24
+
+        # Extract the open restaurant from the dataframe.
+        open1 = df[after_opens1 & before_closes1]
+        open2 = df[after_opens2 & before_closes2]
+
+        # Combine and return the results.
+        all_open = set(open1.loc[:,"Restaurant Name"])
+        all_open = all_open.union(open2.loc[:,"Restaurant Name"])
+        return list(all_open)
